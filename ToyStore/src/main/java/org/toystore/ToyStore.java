adbff7abb775db32106a8764408d094ae7917921
JohnyWest <bbassed@gmail.com>
2023-11-11 23:34:17 +0500

    Add Toy selection + random

--- ToyStore/src/main/java/org/toystore/ToyStore.java
+++ ToyStore/src/main/java/org/toystore/ToyStore.java
@@ -1,5 +1,23 @@
 package org.toystore;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToyStore {
+    private List<Toy> toys;
+    public ToyStore() {
+        toys = new ArrayList<>();
+    }
+    public void addToy(Toy toy) {
+        toys.add(toy);
+    }
+    public void updateWeight(int toyID, int weight){
+        for (Toy toy : toys) {
+            if (toy.getId() == toyID) {
+                toy.setWeight(weight);
+                break;
+            }
+        }
+    }
 
 }
