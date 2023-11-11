b5a8a313ccf77e5df544d9d123af023a6d5b860f
JohnyWest <bbassed@gmail.com>
2023-11-11 23:15:18 +0500

    rework of Toy class

--- ToyStore/src/main/java/org/toystore/Toy.java
+++ ToyStore/src/main/java/org/toystore/Toy.java
@@ -1,14 +1,35 @@
 package org.toystore;
 
 public class Toy {
-    private String id;
+    private int id;
     private String name;
-    private int frequency;
+    private int quantity;
+    private int weight;
 
-    public Toy(String id, String name, int frequency) {
+
+    public Toy(int id, String name, int quantity, int weight ) {
         this.id = id;
         this.name = name;
-        this.frequency = frequency;
+        this.quantity= quantity;
+        this.weight = weight;
     }
     /* Add here getters and setters for fields */
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 }
